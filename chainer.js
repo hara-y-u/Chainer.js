35a320625944d666eb0e2a908486fe81915725e1
yukihiro hara <yukihr@gmail.com>
2012-01-28 21:43:05 +0900

    initial

---
+++ chainer.js
@@ -0,0 +1,239 @@
+/**
+ * Chainer.js - Collection Processing with Chain Methods
+ *   Author:  @yukihr
+ *   License: MIT - http://www.opensource.org/licenses/mit-license
+ */
+window.Chainer = (function() {
+
+  var Chainer = function() {};
+
+  /**
+   * Extend the Object with another Object's properties.
+   *
+   *     @param {Object} base Object to be extended
+   *     @param {Object} ext  Object to extend `base` with its properties
+   */
+  Chainer.extend = function(base, ext) {
+    // algorithm basing jQuery.extend
+    var name
+    , src, copy
+    ;
+    // Extend the base object
+    for(name in ext) {
+      src = base[name];
+      copy = ext[name];
+
+      // Prevent never-ending loop
+      if(base === copy) {
+        continue;
+      }
+
+      if(copy !== undefined) {
+        base[ name ] = copy;
+      }
+    }
+
+    return base;
+  };
+
+  Chainer.extend(Chainer, {
+    /**
+      * If given value is Array-like object (collection), convert it to array,
+      * else return [ value ].
+      *
+      *     @param {Whatever} value
+      */
+    makeArray: function( value ) {
+      // algorithm basing jQuery.makeArray
+      var ret = [], class2type = {}, i
+      , classes = "Boolean Number String Function Array Date RegExp Object".split(" ")
+      ;
+
+      for(i = classes.length; i--;) {
+        class2type[ "[object " + classes[i] + "]" ] = classes[i].toLowerCase();
+      }
+
+      function type( obj ) {
+        return obj == null ?
+          String( obj ) :
+          class2type[ Object.prototype.toString.call(obj) ] || "object";
+      }
+
+      function isWindow( obj ) {
+        return obj && typeof obj === "object" && "setInterval" in obj;
+      }
+
+      if(value != null) {
+        var t = type(value);
+
+        if(value.length == null
+           || t === "string"
+           || t === "function"
+           || t === "regexp"
+           || isWindow(value)) {
+          ret.push(value);
+        } else {
+          if(typeof value.length === 'number') {
+            for(i = value.length; i--;) {
+              ret[i] = value[i];
+            }
+          }
+          else {
+            i = 0;
+            while(value[i] !== undefined) {
+              ret[i] = value[i++];
+            }
+          }
+
+        }
+      }
+
+      return ret;
+    }
+
+  });
+
+  Chainer.extend(Chainer.prototype, {
+    // start func
+    start: function(collection) {
+      return Chainer.extend(
+        Chainer.makeArray(collection)
+        , this);
+    }
+
+    , clone: function() {
+      return Chainer.extend([], this);
+    }
+
+    , empty: function() {
+      var newObj = this.clone();
+      newObj.length = 0;
+      return newObj;
+    }
+
+    // utils
+    , makeArray: function() {
+      return Chainer.makeArray(this);
+    }
+
+    , getIterator: function() {
+      var len = this.length
+      , idx = 0
+      , arr = this.makeArray()
+      ;
+      return {
+        hasNext: function() {
+          return idx < len;
+        }
+        , next: function() {
+          return arr[idx++];
+        }
+        , getIndex: function() {
+          return idx;
+        }
+      };
+    }
+
+    // basics
+    , each: function(callback, thisp) {
+      var it = this.getIterator()
+      ;
+      while(it.hasNext()) {
+        callback.call(thisp || this
+                      , it.next()
+                      , it.getIndex() - 1
+                      , this.makeArray()
+                     );
+      }
+
+      return this;
+    }
+
+    , map: function(callback, thisp) {
+      var ret = this.empty()
+      , that = this
+      ;
+      this.each(function(val, idx, arr) {
+        ret.push(callback.call(thisp || that
+                               , val
+                               , idx
+                               , arr
+                               , ret.makeArray()
+                              ));
+      });
+
+      return ret;
+    }
+
+    , filter: function(callback, thisp) {
+      var ret = this.empty()
+      , that = this
+      ;
+      this.each(function(val, idx, arr) {
+        if(callback.call(thisp || that
+                         , val
+                         , idx
+                         , arr
+                         , ret.makeArray()
+                        )) {
+          ret.push(val);
+        }
+      });
+
+      return ret;
+    }
+
+    , select: function(callback, thisp) {
+      return this.filter(callback, thisp);
+    }
+
+    , reduce: function(callback, initial, thisp) {
+      var ret = initial;
+
+      this.each(function(val, idx, arr) {
+        ret = callback(ret, val, idx, arr, ret);
+      });
+
+      return ret;
+    }
+
+    , reduceRight: function(callback, initial, thisp) {
+      return this;
+    }
+
+    , every: function(callback, thisp) {
+      this.each(function(val, idx, arr) {
+      });
+      return this;
+    }
+
+    , some: function(callback, thisp) {
+      return this;
+    }
+
+    , flatten: function() {
+    }
+
+    // recursive
+    , rmap: function(callback, thisp, indexes) {
+
+      console.log(this,callback,thisp,indexes);
+
+      function isCollection(arg) {
+        return (typeof arg.length !== 'undefined');
+      }
+
+      if(!isCollection(this)) {
+        return callback.call(thisp || this, this, indexes, this);
+      } else {
+        return this.map.call(this, function(val, idx){
+          this.rmap.call(Chainer.extend(val, this), callback, this, idx);
+        }, this);
+      }
+
+    }
+  });
+
+  return Chainer;
+
+}());
---
+++ test/test.js
@@ -0,0 +1,61 @@
+/*global Chainer: false, module: false, test:false, equal: false, deepEqual: false, ok: false*/
+
+$(document).ready(function() {
+
+  module('Core');
+
+  test('Chainer.extend', function() {
+    var base = { foo:1, bar:2, baz:3}, ext = { foo:4, bar:5 };
+
+    Chainer.extend(base, ext);
+
+    deepEqual(base, { foo:4, bar:5, baz:3 });
+  });
+
+  test('Chainer.makeArray', function() {
+    ok(Chainer.makeArray(document.getElementsByTagName('div')) instanceof Array);
+  });
+
+
+  module('Methods');
+
+  test('map', function() {
+    var ch = new Chainer();
+
+    deepEqual(ch.start([1,2,3,4]).map(function(v,i) { return v + i; }).makeArray(), [1,3,5,7], 'use map function');
+  });
+
+
+  module('Extending');
+
+  test('basic', function() {
+    var ch =new Chainer();
+
+    ch.pp = function() {
+      return this.map(function(v) {
+        return v + 1;
+      });
+    };
+
+    deepEqual(ch.start([1,2,3,4]).pp().pp(), [3,4,5,6], 'basic extending');
+  });
+
+
+  module('Inheritance');
+
+  test('basic', function() {
+    function MyChainer() {}
+
+    MyChainer.prototype = new Chainer();
+
+    MyChainer.prototype.pp = function() {
+      return this.map(function(v) {
+        return v + 1;
+      });
+    };
+
+    var ch = new MyChainer();
+    deepEqual(ch.start([1,2,3,4]).pp().pp(), [3,4,5,6], 'basic inheritance');
+  });
+
+});
