9d02efc94e89a7cf3b37df84c638c44d1ace3689
yukihiro hara <yukihr@gmail.com>
2012-01-30 11:44:16 +0900

    test for filter method

--- test/test.js
+++ test/test.js
@@ -15,8 +15,8 @@ $(document).ready(function() {
   test('Chainer.makeArray', function() {
     var testdiv = document.getElementById('core-makearray-test');
     var divs = testdiv.getElementsByTagName('div');
-    ok(Chainer.makeArray(divs) instanceof Array);
-    equal(Chainer.makeArray(divs).length, 3);
+    ok(Chainer.makeArray(divs) instanceof Array, 'return value is Array instance');
+    equal(Chainer.makeArray(divs).length, 3, 'return value has length property with correct value');
   });
 
 
@@ -25,7 +25,13 @@ $(document).ready(function() {
   test('map', function() {
     var ch = new Chainer();
 
-    deepEqual(ch.start([1,2,3,4]).map(function(v,i) { return v + i; }).makeArray(), [1,3,5,7], 'use map function');
+    deepEqual(ch.start([1,2,3,4]).map(function(v,i) { return v + i; }).makeArray(), [1,3,5,7]);
+  });
+
+  test('filter', function() {
+    var ch = new Chainer();
+
+    deepEqual(ch.start([1,2,3,4]).filter(function(v,i) { return !(v%2); }).makeArray(), [2,4]);
   });
 
 
